fc377727bcc9fef17b9244576d6909ec8006f54e
Mateusz Ligeza <m.ligeza@iceo.co>
2020-06-05 22:09:05 +0200

    added thread factory

--- db/src/it/scala/com/mforest/example/db/AsyncIOSpec.scala
+++ db/src/it/scala/com/mforest/example/db/AsyncIOSpec.scala
@@ -4,8 +4,12 @@ import cats.effect.testing.scalatest.{AssertingSyntax, EffectTestSupport}
 import cats.effect.{ContextShift, IO, Timer}
 import org.scalatest.AsyncTestSuite
 
+import scala.concurrent.ExecutionContext
+
 trait AsyncIOSpec extends AssertingSyntax with EffectTestSupport {
   this: AsyncTestSuite =>
+
+  override val executionContext: ExecutionContext = ExecutionContext.global
 
   implicit val ioContextShift: ContextShift[IO] = IO.contextShift(executionContext)
   implicit val ioTimer: Timer[IO] = IO.timer(executionContext)
